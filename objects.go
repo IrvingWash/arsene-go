be0c8694ecedfea9a80a1fed7a63f58deb63b859
Armen Gulakhszyan <agulakhszyan@tradingview.com>
2023-01-16 23:48:56 +0300

    add common objects

---
+++ objects.go
@@ -0,0 +1,15 @@
+package main
+
+type AlbumMetainfo struct {
+	Title       string
+	Artist      string
+	ReleaseYear uint
+	Tracks      []TrackMetainfo
+	AlbumArtURL string
+}
+
+type TrackMetainfo struct {
+	Title       string
+	TrackNumber uint
+	URL         string
+}
